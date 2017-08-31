de29472f58d5d6fa0cb566c1e6d9013719b247c6
Israel <213withani@gmail.com>
2017-08-30 19:49:52 -0700

    Working truncate word based on args

--- truncate_a_string.js
+++ truncate_a_string.js
@@ -2,9 +2,7 @@ function truncateString(str, num) {
   // Clear out that junk in your trunk
   if (str.length===num || str.length<num)
     return str;
-    //runcate a string (first argument) if it is longer than the given
-    //maximum string length (second argument).
-    //Return the truncated string with a ... ending.
+
   if (str.length >num && str.length>3 && num >=3)
     return str.substr(0,num-3).concat("...");
 
