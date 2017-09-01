6fa5abb4e5adf7ca7fa9dfcda9682b34d8cb6917
iluciano <iluciano@iluciano.local>
2017-08-31 17:43:25 -0700

    Working when array length is not even

--- chuncky_monkey.js
+++ chuncky_monkey.js
@@ -1,17 +1,15 @@
-
 function chunkArrayInGroups(arr, size) {
   // Break it up.
   var cp = arr;
-  var newArr=new Array();
+  var newArr=[];
   var s=size,max=arr.length/size;
 
 	newArr.push(cp.slice(0,size));
-
     for(var i=0; i< max-1;i++){
         newArr.push(cp.slice(s,s+size));
-		s+=s;
+		s+=size;
     }
   return newArr;
 }
 
-chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2);
