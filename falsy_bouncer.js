1529230fd088cf07e1e0821696494735dcb23850
iluciano <iluciano@iluciano.local>
2017-09-03 13:54:23 -0700

    corrected lint warnings

--- falsy_bouncer.js
+++ falsy_bouncer.js
@@ -1,10 +1,10 @@
 function bouncer(arr) {
   // Don't show a false ID to this bouncer.
-    var noFalsyValues = arr.filter(function(word){
-		var f=new Boolean(word);
-		console.log(f);
-      return f==true;
-    });
+  var noFalsyValues = arr.filter(function (word) {
+    var f = new Boolean(word);
+    console.log(f);
+    return f === true;
+  });
 
   return noFalsyValues;
 }
